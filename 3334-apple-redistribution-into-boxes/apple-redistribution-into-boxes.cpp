fb68e880dace090ee16da652e1d822e95d173747
Rahul Das <rd2432007@gmail.com>
2025-12-24 12:37:16 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (45.84%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
-        int totalApple=0;
-        for(int i=0;i<apple.size();i++){ //O(n)
-            totalApple+=apple[i];
-        }
-        int ans=0,bucket=0;
+        int totalApple = 0;
+        for (int a : apple) totalApple += a;
         sort(capacity.begin(), capacity.end(), greater<int>());
-        for(int i=0;i<capacity.size();i++){
-            if(totalApple <=bucket){
-                break;
-            }
-            bucket+=capacity[i];
+        int bucket = 0, ans = 0;
+        for (int c : capacity) {
+            if (bucket >= totalApple) break;
+            bucket += c;
             ans++;
         }
         return ans;
