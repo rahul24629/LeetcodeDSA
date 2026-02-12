8deccd3891cd1086c10c7140756cfbf4f0a48c0a
Rahul Das <rd2432007@gmail.com>
2026-02-12 12:41:04 +0530

    Time: 2 ms (68.94%) | Memory: 17.1 MB (95.94%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> dist(n,1e9);
+        dist[src]=0;
+        for(int i=0;i<=k;i++){
+            vector<int>temp=dist;
+            for(auto &f:flights){
+                int u=f[0];
+                int v=f[1];
+                int w=f[2];
+                if(dist[u]!=1e9){
+                    temp[v]=min(temp[v],dist[u]+w);
+                }
+            }
+            dist=temp;
+        }
+        return dist[dst]==1e9?-1:dist[dst];
+        
+    }
+};
