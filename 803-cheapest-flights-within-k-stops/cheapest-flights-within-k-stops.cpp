6b7c73e73321da83a5a8595921d7a75ee5a3367c
Rahul Das <rd2432007@gmail.com>
2026-02-13 19:37:32 +0530

    Time: 7 ms (29.71%) | Memory: 17 MB (95.90%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,21 +1,35 @@
 class Solution {
+    class Info{
+        public:
+        int u;
+        int cost;
+        int stops;
+        Info(int u,int cost,int stops){
+            this->u=u;
+            this->cost=cost;
+            this->stops=stops;
+        }
+    };
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n,1e9);
+        queue<Info>q;
+        vector<int>dist(n,INT_MAX);
         dist[src]=0;
-        for(int i=0;i<=k;i++){
-            vector<int>temp=dist;
-            for(auto &f:flights){
-                int u=f[0];
-                int v=f[1];
-                int w=f[2];
-                if(dist[u]!=1e9){
-                    temp[v]=min(temp[v],dist[u]+w);
+        q.push(Info(src,0,-1));
+        while(!q.empty()){
+            Info curr=q.front();
+            q.pop();
+            for(int i=0;i<flights.size();i++){
+                if(flights[i][0]==curr.u){
+                    int v=flights[i][1];
+                    int wt=flights[i][2];
+                    if(dist[v]>curr.cost+wt && curr.stops+1<=k){
+                        dist[v]=curr.cost+wt;
+                        q.push(Info(v,dist[v],curr.stops+1));
+                    }
                 }
             }
-            dist=temp;
         }
-        return dist[dst]==1e9?-1:dist[dst];
-        
+        return dist[dst]==INT_MAX?-1:dist[dst];
     }
 };
