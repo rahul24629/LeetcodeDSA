f68acd32866bb61f8cf868e8f16ebd49461b2dff
Rahul Das <rd2432007@gmail.com>
2026-03-24 13:10:16 +0530

    Time: 54 ms (76.25%) | Memory: 173.7 MB (97.55%) - LeetSync

---
+++ 4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        if (k == 0) return n;
+
+        sort(nums.begin(), nums.end());
+
+        int threshold = nums[n - k];
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < threshold)
+                count++;
+        }
+
+        return count;
+    }
+};
