022ad0ad012555125dea422d717250a1e11c75de
Rahul Das <rd2432007@gmail.com>
2026-04-27 19:02:04 +0530

    Time: 4 ms (70.00%) | Memory: 11.4 MB (81.00%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         if(s.size()<t.size()) return "";
-
         unordered_map<char,int> mp;
         for(int i=0;i<t.size();i++){
             mp[t[i]]++;
         }
-
         string ans="";
-
         int count=0;
         int flag=0;
         int flag2=0;
         int j=0;
         int ans2=INT_MAX;
-
         for(int i=0;i<s.size();i++){  
             if(mp.find(s[i])!=mp.end()){
                 mp[s[i]]--;
                 if(mp[s[i]]==0) count++;
             }
-
             while(count == mp.size()){  
                 if(ans2 > i-flag+1){
                     ans2 = i-flag+1;   
@@ -37,7 +32,6 @@ public:
                 flag++;
             }
         }
-
         if(ans2 == INT_MAX) return "";
         for(int i=flag2;i<=j;i++){
             ans+=s[i];
