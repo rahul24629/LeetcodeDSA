9fc5c9356af683fbbd06b8014a135cb165014972
Rahul Das <rd2432007@gmail.com>
2026-08-09 11:26:57 +0530

    Time: 64 ms (75.16%) | Memory: 223.7 MB (45.38%) - LeetSync

---
+++ 4363-minimum-total-price-after-applying-discounts/minimum-total-price-after-applying-discounts.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double minPrice(vector<int>& prices, vector<int>& discounts) {
+        sort(prices.begin(), prices.end());
+        sort(discounts.begin(), discounts.end());
+
+        int i=prices.size()-1;
+        int j=discounts.size()-1;
+
+        double ans=0;
+
+        while(i >= 0 && j  >= 0){
+            double finalPrice = (prices[i] * (100 - discounts[j])) / 100.0;
+            ans+= finalPrice;
+            i--;
+            j--;
+        }
+
+        while(i >= 0){
+            ans+=prices[i];
+            i--;
+        }
+
+        return ans;
+        
+    }
+};
